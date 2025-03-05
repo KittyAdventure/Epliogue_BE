b8eb80001ce0d6b3aadf13b322405495de4d9b47
LeeSooBin <romaydev@naver.com>
2025-03-05 17:45:03 +0900

    feat : 인기검색어 기능 구현중

--- src/main/java/com/team1/epilogue/EpilogueApplication.java
+++ src/main/java/com/team1/epilogue/EpilogueApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableJpaAuditing
--- src/main/java/com/team1/epilogue/config/SecurityConfig.java
+++ src/main/java/com/team1/epilogue/config/SecurityConfig.java
@@ -91,7 +91,8 @@ public class SecurityConfig {
                             "/api/books/**",
                             "/api/kp/success",
                             "/api/kp/fail",
-                            "/api/kp/cancel"
+                            "/api/kp/cancel",
+                            "/api/keywords"
                     ).permitAll()
                     .anyRequest().authenticated()
             )
