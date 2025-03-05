7ebae50aac4c3918c03f354e8205483fc65a5ac9
LeeSooBin <romaydev@naver.com>
2025-03-05 12:00:31 +0900

    feat : 댓글 삭제기능 구현. 테스트코드도 작성하였습니다

--- src/main/java/com/team1/epilogue/comment/controller/CommentController.java
+++ src/main/java/com/team1/epilogue/comment/controller/CommentController.java
@@ -8,9 +8,11 @@ import java.util.HashMap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,7 +29,7 @@ public class CommentController {
    * @return 성공시 String 값 응답
    */
   @PostMapping("/api/comments")
-  public ResponseEntity<HashMap<String,String>> postComment(Authentication authentication,
+  public ResponseEntity<HashMap<String, String>> postComment(Authentication authentication,
       @RequestBody CommentPostRequest dto) {
     Member member = (Member) authentication.getPrincipal();
     commentService.postComment(member, dto);
@@ -38,15 +40,39 @@ public class CommentController {
     return ResponseEntity.ok(map);
   }
 
+  /**
+   * 댓글 수정하는 메서드입니다
+   *
+   * @param authentication 인증된 사용자정보
+   * @param dto            수정하려는 댓글 정보가 담긴 DTO
+   */
   @PutMapping("/api/comments")
-  public ResponseEntity<?> updateComment(Authentication authentication,@RequestBody CommentUpdateRequest dto) {
+  public ResponseEntity<HashMap<String, String>> updateComment(Authentication authentication,
+      @RequestBody CommentUpdateRequest dto) {
     Member member = (Member) authentication.getPrincipal();
-    commentService.updateComment(member,dto);
+    commentService.updateComment(member, dto);
 
     HashMap<String, String> map = new HashMap<>();
     map.put("message", "댓글이 성공적으로 작성 되었습니다");
 
     return ResponseEntity.ok(map);
+  }
 
+  /**
+   * 댓글 삭제기능
+   *
+   * @param authentication 인증된 사용자정보
+   * @param commentId      삭제하려는 댓글 정보가 담긴 DTO
+   */
+  @DeleteMapping("/api/comments")
+  public ResponseEntity<?> deleteComment(Authentication authentication,
+      @RequestParam Long commentId) {
+    Member member = (Member) authentication.getPrincipal();
+    commentService.deleteComment(member, commentId);
+
+    HashMap<String, String> map = new HashMap<>();
+    map.put("message", "댓글이 성공적으로 삭제 되었습니다");
+
+    return ResponseEntity.ok(map);
   }
 }
--- src/test/java/com/team1/epilogue/comment/service/CommentServiceTest.java
+++ src/test/java/com/team1/epilogue/comment/service/CommentServiceTest.java
@@ -8,6 +8,7 @@ import com.team1.epilogue.book.entity.Book;
 import com.team1.epilogue.comment.dto.CommentPostRequest;
 import com.team1.epilogue.comment.dto.CommentUpdateRequest;
 import com.team1.epilogue.comment.entity.Comment;
+import com.team1.epilogue.comment.exception.CommentNotFoundException;
 import com.team1.epilogue.comment.exception.UnauthorizedMemberException;
 import com.team1.epilogue.comment.repository.CommentRepository;
 import com.team1.epilogue.review.entity.Review;
@@ -137,5 +138,61 @@ class CommentServiceTest {
     // 캡처된 객체 확인
     Comment capturedComment = commentCaptor.getValue();
     assertEquals("테스트2", capturedComment.getContent());
+  }
+
+  @Test
+  @DisplayName("댓글 삭제기능 테스트")
+  void deleteComment() {
+    //given
+    Long commentId = 1L;
+    Comment comment = Comment.builder()
+        .id(1L)
+        .member(member)
+        .content("테스트 댓글입니다.")
+        .build();
+    when(commentRepository.findById(commentId)).thenReturn(Optional.of(comment));
+
+    // when
+    assertDoesNotThrow(() -> commentService.deleteComment(member, commentId));
+
+    //then
+    verify(commentRepository, times(1)).delete(comment);
+  }
+
+  @DisplayName("댓글 삭제기능 실패 테스트 - 존재하지 않는 댓글")
+  @Test
+  void deleteComment_fail_commentNotFound() {
+    // given
+    when(commentRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+
+    // when & then
+    assertThrows(CommentNotFoundException.class,
+        () -> commentService.deleteComment(member, 1L));
+
+  }
+
+  @DisplayName("댓글 삭제기능 실패 테스트 - 본인의 댓글이 아닐때")
+  @Test
+  void deleteComment_fail_UnAuthorized() {
+    // given
+    Long commentId = 1L;
+    Member member2 = Member
+        .builder()
+        .loginId("empty")
+        .id(2L)
+        .build();
+    Comment comment = Comment.builder()
+        .id(1L)
+        .member(member)
+        .content("테스트 댓글입니다.")
+        .build();
+
+    when(commentRepository.findById(commentId)).thenReturn(Optional.of(comment));
+
+    // when & then
+
+    // deleteComment() 메서드의 작성자가 아닌 다른 사용자의 정보를 넣음
+    assertThrows(UnauthorizedMemberException.class,
+        () -> commentService.deleteComment(member2, commentId));
   }
 }
