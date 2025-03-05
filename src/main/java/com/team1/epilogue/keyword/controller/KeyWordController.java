c6601a6525ff0710a48abdbf072f91ba95e39134
LeeSooBin <romaydev@naver.com>
2025-03-06 01:58:12 +0900

    feat : 인기검색어 기능 구현

--- src/main/java/com/team1/epilogue/keyword/controller/KeyWordController.java
+++ src/main/java/com/team1/epilogue/keyword/controller/KeyWordController.java
@@ -1,6 +1,7 @@
 package com.team1.epilogue.keyword.controller;
 
 import com.team1.epilogue.keyword.service.KeyWordService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class KeyWordController {
   private final KeyWordService keyWordService;
 
+  /**
+   * 인기 검색어를 받아오는 기능입니다. 10개까지 return 합니다
+   */
   @GetMapping("/api/keywords")
   public ResponseEntity<?> getHotKeywords() {
-    keyWordService.getHotKeywords();
+    List<String> hoyKeyWords = keyWordService.getPopularKeywords();
+    return ResponseEntity.ok(hoyKeyWords);
   }
-
-
-
 }
--- src/main/java/com/team1/epilogue/keyword/service/KeyWordService.java
+++ src/main/java/com/team1/epilogue/keyword/service/KeyWordService.java
@@ -1,10 +1,15 @@
 package com.team1.epilogue.keyword.service;
 
-import com.team1.epilogue.keyword.repository.KeyWordRepository;
-import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,23 +17,53 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class KeyWordService {
 
-  private final KeyWordRepository keyWordRepository;
+  private final StringRedisTemplate stringRedisTemplate;
+  public static final long TTL_SECONDS =  86400; // 24 시간
+  private static final String SEARCH_KEY_PREFIX = "search:keyword:";
 
-  public void getHotKeywords() {
-    keyWordRepository.find
+  /**
+   * 검색된 Keyword 를 Redis 에 저장합니다
+   * @param keyWord 검색어
+   */
+  public void saveKeyWord(String keyWord) {
+    // Redis 키: search:keyword:{검색어}:{시간}
+    String redisKey = "search:keyword:" + keyWord;
 
+    // 위 Key 에 해당하는 Value 를 +1 한다.
+    stringRedisTemplate.opsForValue().increment(redisKey, 1);
+    // 만료 시간 설정
+    stringRedisTemplate.expire(redisKey, TTL_SECONDS, TimeUnit.SECONDS);
   }
 
   /**
-   * 어제 00:00 ~ 23:59 동안 수집된 검색어들을 삭제해준다.
-   * 이 작업을 다음날 12:00 에 진행.
-   * 즉 12:00 부터는 당일 00:00 ~ 의 데이터만 수집함
+   * 가장 많이 검색된 키워드를 10개까지 List 로 return 합니다
+   * @return 10개의 인기 검색어
    */
-  @Scheduled(cron = "0 0 12 * * ?") // 매일 낮 12시 실행
-  public void removeOldData() {
-    LocalDateTime today = LocalDateTime.now().toLocalDate().atStartOfDay();
-    keyWordRepository.deleteByCreatedAtBefore(today);
+  public List<String> getPopularKeywords() {
+    // redis 에서 검색어들을 가져온다
+    Set<String> keys = stringRedisTemplate.keys(SEARCH_KEY_PREFIX + "*");
 
-    log.info("인기검색어 DB 내부 어제날짜 데이터들이 삭제 되었습니다.");
+    if (keys == null || keys.isEmpty()) {
+      return Collections.emptyList();
+    }
+
+    Map<String, Integer> keywordCounts = new HashMap<>();
+
+    for (String key : keys) {
+      // keyWord 만 가져온다
+      String keyword = key.replace(SEARCH_KEY_PREFIX, "");
+      log.info(keyword);
+      // redis 내부 value 를 가져온다
+      String countStr = stringRedisTemplate.opsForValue().get(key);
+      int count = (countStr != null) ? Integer.parseInt(countStr) : 0;
+      keywordCounts.put(keyword, count);
+    }
+
+    // 10개의 검색어만 value 의 숫자가 높은 순서대로 return
+    return keywordCounts.entrySet().stream()
+        .sorted((a, b) -> Integer.compare(b.getValue(), a.getValue()))
+        .limit(10)
+        .map(Map.Entry::getKey)
+        .collect(Collectors.toList());
   }
 }
