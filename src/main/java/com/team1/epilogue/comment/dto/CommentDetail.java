a24f4158e1d24024a75b98ba417563d06698c8ed
LeeSooBin <romaydev@naver.com>
2025-03-11 13:05:51 +0900

    refactor : CommentDetail 내부 @JsonFormat 추가

--- src/main/java/com/team1/epilogue/comment/dto/CommentDetail.java
+++ src/main/java/com/team1/epilogue/comment/dto/CommentDetail.java
@@ -1,5 +1,6 @@
 package com.team1.epilogue.comment.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,6 +16,7 @@ public class CommentDetail {
   private long memberId;
   private String memberNickname;
   private String memberProfile;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm:ss")
   private LocalDateTime commentPostDateTime;
   private int commentLike;
   private String commentColor;
