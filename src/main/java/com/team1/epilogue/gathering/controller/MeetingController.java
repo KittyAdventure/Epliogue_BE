feece05ff4bd13c1f48ab1563e1d29020946b0d4
KorMH <dvkk12@gmail.com>
2025-03-12 21:07:48 +0900

    fix : 모임 조회 ,테스트 코드 작성

--- src/main/java/com/team1/epilogue/gathering/controller/MeetingController.java
+++ src/main/java/com/team1/epilogue/gathering/controller/MeetingController.java
@@ -5,12 +5,18 @@ import com.team1.epilogue.gathering.dto.MeetingDto;
 import com.team1.epilogue.gathering.entity.Meeting;
 import com.team1.epilogue.gathering.service.MeetingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,5 +38,17 @@ public class MeetingController {
     return ResponseEntity.ok(meetingService.updateMeeting(id, dto));
   }
 
+  // 오프라인 모임 조회
+  @GetMapping
+  public ResponseEntity<Page<MeetingDto>> getMeetings(
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "10") int size
+  ) {
+
+    Pageable pageable = PageRequest.of(page,size, Sort.by("createdAt").descending());
+    Page<MeetingDto> meetingDtoPage = meetingService.getMeetings(pageable);
+
+    return ResponseEntity.ok(meetingDtoPage);
+  }
 
 }
